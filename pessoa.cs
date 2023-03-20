1e83a749f09e89399c9adcd2951c4b0ed4fbf690
ScheilaAlbino <scheilasilvaalbino@rede.ulbra.br>
2023-03-20 17:54:56 -0300

    envio tde

---
+++ pessoa.cs
@@ -0,0 +1,15 @@
+namespace aula04Tde
+{
+    public class Pessoa
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;} = " ";
+        public string Telefone {get; set;}
+
+        public Pessoa(){
+            Id = Id;
+            Nome = Nome;
+            Telefone = Telefone;
+        }
+    }    
+}
